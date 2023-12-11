1ab51d3abacfec8f4ddb619903ebdd494caf2616
Szer13 <shad350@gmail.com>
2023-12-11 12:35:07 +0100

    e3d V6 hotend

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -680,14 +680,14 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  13.74
-    #define DEFAULT_Ki  0.72
-    #define DEFAULT_Kd  65.51
+    #define DEFAULT_Kp  25.44
+    #define DEFAULT_Ki  2.32
+    #define DEFAULT_Kd  69.70
   #endif
 #else
   #define BANG_MAX 255    // Limit hotend current while in bang-bang mode; 255=full current
 #endif
-
+//M303 E0 S205 C8 - hotend PID
 /**
  * Model Predictive Control for hotend
  *
